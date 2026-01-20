ad57a0289ecb46fd4f56ebe92fb86416e74f1eb0
Nurdaulet Yerkinbekov <59971266+NurdauletYN@users.noreply.github.com>
2026-01-20 18:24:59 +0500

    Add root layout

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Wizard Chess",
+  description: "Wizard Chess official site",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0 }}>{children}</body>
+    </html>
+  );
+}
